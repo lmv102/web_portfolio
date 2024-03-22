324e209ed2d98410245c23cfca8a6d88c371c796
Leah Vaughan <lmv102@msstate.edu>
2024-03-21 23:40:11 -0500

    added files from class

---
+++ script.js
@@ -0,0 +1,11 @@
+// To see if your script properly connected to your index.html
+// Open your website in your browser, go to developer tools 
+// and open the console. 
+// You should see "Hello from Javascript written in the console."
+console.log("Hello from Javascript"); // DELETE ME 
+
+// ADD YOUR toggleMenu() function here
+
+// CODE HERE
+
+// END
